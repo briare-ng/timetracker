ea248781de846598e839068c88be46ec2a7adc72
Briare Nguyen <briare-nguyen@outlook.fr>
2024-08-02 15:51:12 +0200

    responsive correction + add info message

--- components/Home.js
+++ components/Home.js
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useState, useEffect, useRef } from "react";
 import { addActivity } from "../reducers/activities";
 import Activity from "./Activity";
 import styles from "../styles/Home.module.css";
@@ -6,18 +6,31 @@ import { useDispatch, useSelector } from "react-redux";
 
 function Home() {
   const [activityName, setActivityName] = useState("");
+  const [info, setInfo] = useState({});
+  let timer= useRef(null);
 
   const activities = useSelector((state) => state.activities.value);
   const dispatch = useDispatch();
 
   const createActivity = () => {
-    if (activityName !== "") {
+    if (activityName == "") {
+      setInfo({ msg: "Veuillez entrer un nom d'activité", color: "red" });
+      return;
+    }
+    if (!activities.some((activity) => activity.name === activityName)) {
       dispatch(addActivity(activityName));
       setActivityName("");
+      setInfo({ msg: "Activité ajoutée !", color: "green" });
+    } else {
+      setInfo({ msg: "Le nom de cette activité existe déjà", color: "red" });
     }
   };
-
-  // console.log("activities", activities);
+  useEffect(() => {
+    if (info.msg) {
+      clearTimeout(timer.current);
+      timer.current = setTimeout(() => setInfo({}), 3000);
+    }
+  }, [info]);
 
   const activitiesComponents = activities.map((data, i) => {
     return (
@@ -33,25 +46,29 @@ function Home() {
 
   return (
     <div className={styles.container}>
-      <div className={styles.topSection}>
-        <div className={styles.trackerWindow}>
-          <div className={styles.trackerHeader}>Time tracker</div>
-          <div className={styles.addSection}>
-            <input
-              type="text"
-              placeholder="Activity name"
-              id="activityName"
-              onChange={(e) => setActivityName(e.target.value)}
-              value={activityName}
-            />
-            <button id="add" onClick={createActivity}>
-              Add activity
-            </button>
+      <div className={styles.main}>
+        <div className={styles.topSection}>
+          <div className={styles.trackerWindow}>
+            <div className={styles.trackerHeader}>Time tracker</div>
+            <div className={styles.addSection}>
+              <input
+                type="text"
+                placeholder="Activity name"
+                id="activityName"
+                onChange={(e) => setActivityName(e.target.value)}
+                value={activityName}
+              />
+              <button id="add" onClick={createActivity}>
+                Add activity
+              </button>
+              <p className={styles.infoMsg} style={{ color: info.color }}>
+                {info.msg}
+              </p>
+            </div>
           </div>
         </div>
+        <div className={styles.bottomSection}>{activitiesComponents}</div>
       </div>
-
-      <div className={styles.bottomSection}>{activitiesComponents}</div>
     </div>
   );
 }
